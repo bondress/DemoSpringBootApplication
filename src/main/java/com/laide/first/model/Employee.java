3322abbc29514f5607a512e5aca239473640734a
Laide <endeleylaide@gmail.com>
2021-04-02 08:32:43 +0100

    Initial commit

---
+++ src/main/java/com/laide/first/model/Employee.java
@@ -0,0 +1,48 @@
+package com.laide.first.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    public Employee(long id, String name, int age){
+        super();
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
